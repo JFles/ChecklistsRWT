ad6d89b3cfcc99187e3764391918378e000f2303
Jeremy Fleshman <jfleshman@outsitenetworks.com>
2018-05-10 20:25:13 -0400

    added cells and hardcoded row data to test scrolling with reusable cells. cat-9

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -21,15 +21,39 @@ class ChecklistViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 100
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableView {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: IndexPath)
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
         
+        // tag is used here instead of an @IBOutlet since the element is reusable and has multiple instances
+        let label = cell.viewWithTag(1000) as! UILabel
+        
+        if indexPath.row % 5 == 0 {
+            label.text = "What the Barty Crouch"
+        } else if indexPath.row % 5 == 1 {
+            label.text = "Why lie, son"
+        } else if indexPath.row % 5 == 2 {
+            label.text = "I need more coffee"
+        } else if indexPath.row % 5 == 3 {
+            label.text = "And then it was Eirday"
+        } else if indexPath.row % 5 == 4 {
+            label.text = "At least it's better than auto layout..."
+        }
+
         return cell
     }
 
-
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if let cell = tableView.cellForRow(at: indexPath) {
+            if cell.accessoryType == .none {
+                cell.accessoryType = .checkmark
+            } else {
+                cell.accessoryType = .none
+            }
+        }
+    }
 }
-
